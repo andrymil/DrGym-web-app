4a7ed117c910392ded8a4827e829f77c6436cf72
mandrysz <01187204@pw.edu.pl>
2025-03-20 18:48:15 +0000

    configured schema

---
+++ src/app/api/exercises/route.js
@@ -0,0 +1,14 @@
+import prisma from '@prisma';
+
+export async function GET() {
+  try {
+    const exercises = await prisma.exercises.findMany();
+    return Response.json(exercises, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching exercises:', error);
+    return Response.json(
+      { error: 'Failed to fetch exercises' },
+      { status: 500 }
+    );
+  }
+}
