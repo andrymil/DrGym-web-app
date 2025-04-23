9a9e031dc38542ae13a122696997d4433fbaaaa4
mandrysz <01187204@pw.edu.pl>
2025-04-23 23:54:37 +0200

    added crypto util file

---
+++ src/utils/crypto.js
@@ -0,0 +1,17 @@
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
+
+export const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 12);
+};
+
+export const verifyPassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+export const generateToken = () => {
+  const token = crypto.randomBytes(32).toString('hex');
+  const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+  return { token, hashedToken };
+};
