cfdf0801a19cc8e29e09d63f6f4698d4d4cc59aa
mandrysz <01187204@pw.edu.pl>
2025-05-13 00:19:15 +0200

    fixed sendEmail

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,24 +1,42 @@
 'use server';
 
-import nodemailer from 'nodemailer';
+import nodemailer, { TestAccount } from 'nodemailer';
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 import { Eta } from 'eta';
 import path from 'path';
 import open from 'open';
+
+interface EmailTemplateData {
+  subject: string;
+  [key: string]: unknown;
+}
+
+interface SendEmailArgs {
+  to: string;
+  templateName: string;
+  templateData: EmailTemplateData;
+}
 
 const eta = new Eta({
   views: path.join(process.cwd(), 'src', 'emails'),
 });
 
-const testAccount = await nodemailer.createTestAccount();
+const testAccount: TestAccount = await nodemailer.createTestAccount();
 
-export default async function sendEmail({ to, templateName, templateData }) {
-  const body = eta.render(`${templateName}/template.eta`, templateData);
-  const html = eta.render('layouts/base.eta', { ...templateData, body });
-  const textBody = eta.render(`${templateName}/template.txt.eta`, templateData);
-  const text = eta.render('layouts/base.txt.eta', {
-    ...templateData,
-    body: textBody,
-  });
+export default async function sendEmail({
+  to,
+  templateName,
+  templateData,
+}: SendEmailArgs): Promise<void> {
+  const body = eta.render(`${templateName}/template.eta`, templateData) ?? '';
+  const html = eta.render('layouts/base.eta', { ...templateData, body }) ?? '';
+  const textBody =
+    eta.render(`${templateName}/template.txt.eta`, templateData) ?? '';
+  const text =
+    eta.render('layouts/base.txt.eta', {
+      ...templateData,
+      body: textBody,
+    }) ?? '';
 
   const transporter = nodemailer.createTransport({
     host: testAccount.smtp.host,
@@ -38,7 +56,8 @@ export default async function sendEmail({ to, templateName, templateData }) {
     text,
   };
 
-  const info = await transporter.sendMail(mailOptions);
+  const info: SMTPTransport.SentMessageInfo =
+    await transporter.sendMail(mailOptions);
 
   console.log('Message sent:', info.messageId);
 
@@ -48,13 +67,3 @@ export default async function sendEmail({ to, templateName, templateData }) {
     await open(previewUrl);
   }
 }
-
-//   const transporter = nodemailer.createTransport({
-//     host: testAccount.smtp.host,
-//     port: testAccount.smtp.port,
-//     secure: testAccount.smtp.secure,
-//     auth: {
-//       user: testAccount.user,
-//       pass: testAccount.pass,
-//     },
-//   });
