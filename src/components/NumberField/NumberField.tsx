d5d6199f4ac12fd0459ec7ca00dca57f57dda001
mandrysz <01187204@pw.edu.pl>
2025-05-12 17:13:43 +0200

    created NumberField component

---
+++ src/components/NumberField/NumberField.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TextField, TextFieldProps } from '@mui/material';
+
+type CustomInputProps = TextFieldProps & {
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  setHasChanges?: (value: React.SetStateAction<boolean>) => void;
+};
+
+const CustomInput = ({
+  handleChange,
+  setHasChanges,
+  ...rest
+}: CustomInputProps) => {
+  return (
+    <TextField
+      {...rest}
+      type="number"
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        setHasChanges && setHasChanges(true);
+        const value = e.target.value;
+        if (!value || parseInt(value, 10) >= 0) {
+          handleChange(e);
+        }
+      }}
+      slotProps={{
+        htmlInput: {
+          min: 0,
+          onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === '-' || e.key === 'e') {
+              e.preventDefault();
+            }
+          },
+        },
+      }}
+    />
+  );
+};
+
+export default CustomInput;
